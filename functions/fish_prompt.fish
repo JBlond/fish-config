e9e949cfc9f975f3b74cc6304ca5415bd2c6f2ad
JBlond <leet31337@web.de>
2019-03-14 15:09:05 +0100

    update

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -56,7 +56,11 @@ function fish_prompt
   end
 
   echo -n -s $status_indicator 
-  echo (whoami) $cwd $git_info $normal ' '
+  if [ -n "SSH_TTY" -o -n "$SSH_CLIENT" ]
+    echo (whoami) $cwd $git_info $normal ' '
+  else
+    echo (whoami)@(hostname) $cwd $git_info $normal ' '
+  end
   # echo # To print an empty line
   # prompt character
   set_color ff0000
