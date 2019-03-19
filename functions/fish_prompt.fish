7f3e74804410d9e5abcb972d1522aa3c11643833
JBlond <leet31337@web.de>
2019-03-19 13:14:19 +0100

    design the fish shell more like my bash shell

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -15,7 +15,7 @@ function fish_prompt
   set -l blue (set_color -o blue)
   set -l green (set_color -o green)
   set -l normal (set_color normal)
-  
+
   if not set -q __fish_prompt_char
         switch (id -u)
             case 0
@@ -30,18 +30,18 @@ function fish_prompt
   else
       set status_indicator "$red✗ "
   end
-  set -l cwd $cyan(prompt_pwd)
-  
+  set -l cwd $blue(prompt_pwd)
+
   set -l branch_name (_git_branch_name)
   #echo 'branch name is ' + $branch_name
   if [ $branch_name ]
 
     if test $branch_name = 'master'
       set -l git_branch "master"
-      set git_info "$normal (♆ $red$git_branch$normal)"
+      set git_info \n"$normal $cyan(♆ $red$git_branch)$normal"
     else
       set -l git_branch $branch_name
-      set git_info "$normal (♆ $blue$git_branch$normal)"
+      set git_info \n"$normal $cyan(♆ $git_branch)$normal"
     end
 
     if [ (_is_git_dirty) ]
@@ -55,11 +55,11 @@ function fish_prompt
     echo The last command took (math "$CMD_DURATION/1000") seconds.
   end
 
-  echo -n -s $status_indicator 
+  echo -n -s $status_indicator
   if set -q SSH_TTY
-    echo $red'ssh://'$cyan(whoami)$green'@'(hostname) $blue $cwd $git_info $normal ' '
+    echo $red'ssh://'$cyan(whoami)$green'@'(hostname) $cwd $git_info $normal ' '
   else
-    echo $cyan(whoami) $blue $cwd $git_info $normal ' '
+    echo $cyan(whoami) $cwd $git_info $normal ' '
   end
   # echo # To print an empty line
   # prompt character
