7640acd3fc32092d28c7d51b5332b0f84b40365f
JBlond <leet31337@web.de>
2019-05-02 10:21:50 +0200

    add rns alias

--- config.fish
+++ config.fish
@@ -71,6 +71,7 @@ alias sc='systemctl'
 alias grep='grep --color=auto --exclude-dir="node_modules"'
 alias fgrep='fgrep --color=auto --exclude-dir="node_modules"'
 alias egrep='egrep --color=auto --exclude-dir="node_modules"'
+alias rns='grep -Rins $1 *'
 
 alias download="curl -LO "
 
